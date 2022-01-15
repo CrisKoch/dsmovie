d4b15d3563ce237be7783f384f4f79e46a6a689e
Cristina Kochmann <cristinak.iftm@gmail.com>
2022-01-15 14:49:49 -0300

    Repair

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -7,7 +7,7 @@ function NavBar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https=//github.com/CrisKoch"></a>
+                    <a href="https://github.com/CrisKoch"></a>
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
                         <p className= "dsmovie-contact-link">/devsuperior</p>
